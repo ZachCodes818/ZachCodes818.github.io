5149a7f8da0e69586b4756f1cbb4af89ba1d5006
Zach Jayson <zachjayson@Zachs-MacBook-Air.local>
2019-04-30 14:01:00 -0700

    Adding popup and hamburger menu

--- willie.js
+++ willie.js
@@ -1,20 +1,20 @@
 $('a[href*="#"]')
-  // Remove links that don't actually link to anything
+
   .not('[href="#"]')
   .not('[href="#0"]')
-  .click(function(event) {
+  .click(function (event) {
     // On-page links
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
       location.hostname == this.hostname
     ) {
-      // Figure out element to scroll to
+
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
+
       if (target.length) {
-        // Only prevent default if animation is actually gonna happen
+
         event.preventDefault();
         $('html, body').animate({
           scrollTop: target.offset().top
@@ -22,3 +22,37 @@ $('a[href*="#"]')
       }
     }
   });
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
+  $(function(){
+    $("form").submit(function(e){
+        e.preventDefault();
+        var href = $(this).attr("action");
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: href,
+            data: $(this).serialize(),
+            success: function(response){
+                if(response.status == "success"){
+                  var elems = document.querySelector('.modal');
+                  var instance = M.Modal.init(elems);
+                  instance.open();
+                  setTimeout(function(){
+                    window.location.replace("https://www.jaysoncodes.com");
+                  }, 5000)
+                
+              
+                }else{
+                    alert("An error occured: " + response.message);
+                }
+            }
+        });
+    });
+});
+
